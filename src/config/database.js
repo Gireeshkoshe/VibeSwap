e7c1ca050cf06a91ee7a1753f21eb159fb32c5d9
Gireesh Koshe <164081166+Gireeshkoshe@users.noreply.github.com>
2025-03-07 19:34:43 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose")
 async function connect(){
-    mongoose.connect("mongodb+srv://admin:Ql7ZnqTZM7u7Ja4s@cluster0.hwmccod.mongodb.net/VibeSwap")
+    mongoose.connect()
 }
 
 module.exports=connect;
